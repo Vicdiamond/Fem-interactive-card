cbd2aca7b743ff43264a90c8d7c5643dc21e9eb4
Dev <97352849+Vicdiamond@users.noreply.github.com>
2023-11-15 12:54:40 +0100

    first commit

---
+++ build/index.js
@@ -0,0 +1,121 @@
+'use strict'
+
+const cardName = document.querySelector('.card-name')
+const cardNum = document.querySelector('.card-num')
+const cardMM = document.querySelector('.card-mm')
+const cardYY = document.querySelector('.card-yy')
+const cardCVC = document.querySelector('.card-cvc')
+const mainEl = document.querySelector('.main')
+const completeEl = document.querySelector('.complete')
+
+const btnSubmit = document.querySelector('.btn-submit')
+const btnContinue = document.querySelector('.btn-continue')
+
+// Functions
+
+const containsAlphabet = function (inputValue) {
+  for (let i = 0; i < inputValue.length; i++) {
+    if (
+      (inputValue[i] >= 'a' && inputValue[i] <= 'z') ||
+      (inputValue[i] >= 'A' && inputValue[i] <= 'Z')
+    ) {
+      return true
+    }
+  }
+  return false
+}
+
+const clearInputField = function () {
+  cardName.value = ''
+  cardNum.value = ''
+  cardMM.value = ''
+  cardYY.value = ''
+  cardCVC.value = ''
+}
+const checkEmptyInputs = function () {
+  if (
+    cardName.value === '' ||
+    cardNum.value === '' ||
+    cardMM.value === '' ||
+    cardYY.value === '' ||
+    cardCVC.value === ''
+  ) {
+    return false
+  } else {
+    return true
+  }
+}
+const limitInputLength = function (input, maxLength) {
+  const inputValue = input.value.toString()
+
+  if (inputValue.length > maxLength) {
+    input.value = inputValue.slice(0, maxLength)
+  }
+}
+
+// Event listeners
+document.querySelector('.contain').addEventListener('click', function (e) {
+  const clicked = e.target.closest('.card')
+
+  if (!clicked) return
+
+  clicked.addEventListener('mouseout', function () {
+    if (clicked.value === '') {
+      e.target.classList.add('border-red-500')
+      clicked.nextElementSibling.classList.remove('hidden')
+    } else {
+      e.target.classList.remove('border-red-500')
+      clicked.nextElementSibling.classList.add('hidden')
+    }
+
+    // for the card number
+    if (clicked.classList.contains('card-num')) {
+      const clickedValue = clicked.value
+      console.log(clickedValue)
+      const containsAlphabetChar = containsAlphabet(clickedValue)
+      if (containsAlphabetChar) {
+        clicked.nextElementSibling.textContent = 'Wrong format, numbers only'
+        e.target.classList.add('border-red-500')
+        clicked.nextElementSibling.classList.remove('hidden')
+      } else {
+        e.target.classList.remove('border-red-500')
+        clicked.nextElementSibling.classList.add('hidden')
+      }
+      if (clicked.value === '') {
+        clicked.nextElementSibling.textContent = `Can't be blank`
+        clicked.nextElementSibling.classList.remove('hidden')
+        e.target.classList.add('border-red-500')
+      }
+    }
+  })
+
+  //   Checking required input length
+  if (
+    clicked.classList.contains('card-mm') ||
+    clicked.classList.contains('card-yy')
+  ) {
+    clicked.addEventListener('input', function () {
+      limitInputLength(this, 2)
+    })
+  }
+
+  clicked.classList.contains('card-cvc') &&
+    clicked.addEventListener('input', function () {
+      limitInputLength(clicked, 3)
+    })
+})
+
+btnSubmit.addEventListener('click', function (e) {
+  e.preventDefault()
+  if (checkEmptyInputs()) {
+    mainEl.classList.add('hidden')
+    completeEl.classList.remove('hidden')
+    completeEl.classList.add('flex')
+    clearInputField()
+  }
+})
+
+btnContinue.addEventListener('click', function (e) {
+  mainEl.classList.remove('hidden')
+  completeEl.classList.add('hidden')
+})
